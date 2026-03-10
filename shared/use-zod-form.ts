456da6b2186433662f594e7900cad55c87eeb204
zalaya <enol.corzal@gmail.com>
2026-03-11 00:42:16 +0100

    feat: rename and restructure authentication pages, update clean script in package.json

--- shared/use-zod-form.ts
+++ shared/use-zod-form.ts
@@ -2,8 +2,7 @@ import { Resolver, useForm, UseFormProps, UseFormReturn } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z, ZodObject } from 'zod'
 
-export type ZodFormReturn<TSchema extends ZodObject> =
-  UseFormReturn<z.infer<TSchema>> & {
+export type ZodFormReturn<TSchema extends ZodObject> = UseFormReturn<z.infer<TSchema>> & {
   schema: TSchema
 }
 
