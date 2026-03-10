40dfda1711d11a4b4223cd2bada1e95bd9f8fcf2
Enol Cortina Zalaya <136749654+zalaya@users.noreply.github.com>
2026-03-10 23:00:45 +0100

    fix: remove the form directory from the repository

--- components/ui/form/FormSchemaContext.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-
-import { createContext } from 'react'
-import { ZodObject, ZodRawShape } from 'zod'
-
-export const FormSchemaContext = createContext<ZodObject<ZodRawShape> | undefined>(undefined)
